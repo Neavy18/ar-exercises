548503e023e7f20e126ad197daee5070439a7bf2
Neavy18 <genevieve.gt18@gmail.com>
2022-02-03 13:20:01 -0500

    Add selector for men / women criteria and yearly revenue

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,33 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+s4 = Store.new
+s4.name = "Surrey"
+s4.annual_revenue =  224000
+s4.mens_apparel = false
+s4.womens_apparel = true
+s4.save
+
+s5 = Store.new
+s5.name = "Whistler"
+s5.annual_revenue =  1900000
+s5.mens_apparel = true
+s5.womens_apparel = false
+s5.save
+
+s6 = Store.new
+s6.name = "Yaletown"
+s6.annual_revenue =  430000
+s6.mens_apparel = true
+s6.womens_apparel = true
+s6.save
+
+puts Store.count
+
+@mens_stores = Store.where(mens_apparel: "true")
+@mens_stores.each{ |s| puts "#{s.name}'s annual revenue is #{s.annual_revenue}"}
+
+@womens_stores = Store.where(womens_apparel: "true")
+@womens_stores.each{ |s| puts "#{s.name}'s annual revenue is #{s.annual_revenue}"}
+
+
