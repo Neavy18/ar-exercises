21f729650df9fc292df61087432a1bffde1f21ae
Neavy18 <genevieve.gt18@gmail.com>
2022-02-03 12:47:09 -0500

    Create 3 stores

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+puts @store1 = Store.find_by(id: 1)
+puts @store2 = Store.second
+
+@store1.name = "Eaton"
+@store1.save
+
+puts @store1.name
